6bb38522d4d5b692e114eaa267bca0c3091f1f7d
Truman Gao <mr.trumgao@gmail.com>
2022-06-05 16:02:58 -0700

    implement try login submit interval and option to not use saved login

--- UVicAutoLogin/content.js
+++ UVicAutoLogin/content.js
@@ -1,50 +1,39 @@
-chrome.storage.local.get(['username', 'ps', 'extensionEnabled'], function(login) {
-  if (login.extensionEnabled){
-    // uvicLogin(login.username, login.ps);
+chrome.storage.local.get(['username', 'ps', 'savedLogin'], function(login) {
+  if (login.savedLogin){
+    setLogin(login.username, login.ps);
   }
-
-  console.log('running')
   
+  // submit login form if inputs are not empty
+  // attempt for 15 seconds
   const loginInterval = setInterval(function(){
     const userInput = document.querySelector("#username");
     const psInput = document.querySelector("#password");
 
-    document.querySelector('[for="username"]').click();
-
-    console.log('running')
     if ((userInput.value !== '') && (psInput.value !== '')){
       loginSubmit();
       clearInterval(loginInterval);
     }
+
   }, 300);
 
-  // timeout after 5 seconds of trying
+  // timeout after 15 seconds of login submits
   setTimeout(function(){
-    clearInterval(loginInterval)
-  }, 15000)
+    clearInterval(loginInterval);
+  }, 15000);
   
 });
 
 
-function uvicLogin(username, ps) {
-  // console.log('logging in with:', username, ps);
-
+function setLogin(username, ps) {
   const userInput = document.querySelector("#username");
   if (userInput) userInput.value = username;
+
   const psInput = document.querySelector("#password")
   if (psInput) psInput.value = ps;
 
   const rememberMe = document.querySelector("#rememberMe")
   if (rememberMe) rememberMe.checked = true;
-
-  const submit = document.querySelector("#form-submit")
-  if (submit) submit.click();
 }
-
-function uvicLogin2(){
-
-}
-
 
 function loginSubmit(){
   const submit = document.querySelector("#form-submit")
