76365b89a82b2ebc766b55f41a11be2a9968358b
Truman Gao <mr.trumgao@gmail.com>
2022-06-05 13:17:22 -0700

    implementation working, but needs user click for chrome autofill to input into field

--- UVicAutoLogin/content.js
+++ UVicAutoLogin/content.js
@@ -1,8 +1,28 @@
 chrome.storage.local.get(['username', 'ps', 'extensionEnabled'], function(login) {
   if (login.extensionEnabled){
-    uvicLogin(login.username, login.ps);
-
+    // uvicLogin(login.username, login.ps);
   }
+
+  console.log('running')
+  
+  const loginInterval = setInterval(function(){
+    const userInput = document.querySelector("#username");
+    const psInput = document.querySelector("#password");
+
+    document.querySelector('[for="username"]').click();
+
+    console.log('running')
+    if ((userInput.value !== '') && (psInput.value !== '')){
+      loginSubmit();
+      clearInterval(loginInterval);
+    }
+  }, 300);
+
+  // timeout after 5 seconds of trying
+  setTimeout(function(){
+    clearInterval(loginInterval)
+  }, 15000)
+  
 });
 
 
@@ -17,6 +37,16 @@ function uvicLogin(username, ps) {
   const rememberMe = document.querySelector("#rememberMe")
   if (rememberMe) rememberMe.checked = true;
 
+  const submit = document.querySelector("#form-submit")
+  if (submit) submit.click();
+}
+
+function uvicLogin2(){
+
+}
+
+
+function loginSubmit(){
   const submit = document.querySelector("#form-submit")
   if (submit) submit.click();
 }
