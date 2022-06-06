46452f3f58ea83358d6b0a853da745b4f21c2897
Truman Gao <mr.trumgao@gmail.com>
2022-06-05 17:11:05 -0700

    return to previous implementation

--- UVicAutoLogin/content.js
+++ UVicAutoLogin/content.js
@@ -1,41 +1,19 @@
-chrome.storage.local.get(['username', 'ps', 'savedLogin'], function(login) {
-  if (login.savedLogin){
-    setLogin(login.username, login.ps);
+chrome.storage.local.get(['username', 'password', 'extensionEnabled'], function(login) {
+  if (login.extensionEnabled){
+    uvicLogin(login.username, login.password);
   }
-  
-  // submit login form if inputs are not empty
-  // attempt for 15 seconds
-  const loginInterval = setInterval(function(){
-    const userInput = document.querySelector("#username");
-    const psInput = document.querySelector("#password");
-
-    if ((userInput.value !== '') && (psInput.value !== '')){
-      loginSubmit();
-      clearInterval(loginInterval);
-    }
-
-  }, 300);
-
-  // timeout after 15 seconds of login submits
-  setTimeout(function(){
-    clearInterval(loginInterval);
-  }, 15000);
-  
 });
 
-
-function setLogin(username, ps) {
+function uvicLogin(username, password) {
   const userInput = document.querySelector("#username");
   if (userInput) userInput.value = username;
 
-  const psInput = document.querySelector("#password")
-  if (psInput) psInput.value = ps;
+  const passwordInput = document.querySelector("#password")
+  if (passwordInput) passwordInput.value = password;
 
   const rememberMe = document.querySelector("#rememberMe")
   if (rememberMe) rememberMe.checked = true;
-}
 
-function loginSubmit(){
   const submit = document.querySelector("#form-submit")
   if (submit) submit.click();
 }
